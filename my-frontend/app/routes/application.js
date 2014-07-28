04e12c573399f08472307d2654a46360dc365986
Maximilian Neger <maximilian.neger@gmail.com>
2014-07-28 13:48:52 +0200

    Call super in beforeModel hook
    Use super to call the application routes beforeModel hook so the
    ApplicationRouteMixin works.

--- my-frontend/app/routes/application.js
+++ my-frontend/app/routes/application.js
@@ -4,6 +4,7 @@ import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 export default Ember.Route.extend(
     ApplicationRouteMixin, {
     beforeModel: function () {
+        this._super.apply(this, arguments);
         return this.csrf.fetchToken();
     }
 });
